3cdda59bb13e2970b15c1b9765e196ea4ed801cf
Mo7amedSMahdi <Mo7ammed.salah@outlook.com>
2022-07-05 19:25:00 +0300

    day one setup

---
+++ day-1/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h3>Produc card</h3>
+    </div>
+  );
+}
+
+export default App;
