133dd1d745a4367a159860ffd6b3ddd703addf3b
Mo7amedSMahdi <Mo7ammed.salah@outlook.com>
2022-07-06 10:43:00 +0300

    day-2 finish the product card

--- day-1/src/App.js
+++ day-1/src/App.js
@@ -1,13 +1,25 @@
+import React, { useState, useEffect, useLayoutEffect } from 'react';
 import './App.css';
 import CardImageDesktop from './images/image-product-desktop.jpg';
 import CardImageMobile from './images/image-product-mobile.jpg';
 
 function App() {
+  const [CardImage, SetCardImage] = useState(CardImageMobile);
+  const size = useWindowSize();
+
+  useEffect(() => {
+    if (size.width > 768) {
+      SetCardImage(CardImageDesktop);
+    } else {
+      SetCardImage(CardImageMobile);
+    }
+  }, [size.width]);
+
   return (
     <div className='card-container'>
       <div className='product-card'>
         <div className='card-image'>
-          <img src={CardImageMobile} alt='chanel perfume' />
+          <img src={CardImage} alt='chanel perfume' />
         </div>
         <div className='card-content'>
           <div className='product-details'>
@@ -31,8 +43,8 @@ function App() {
               </div>
             </div>
             <div className='action-button'>
-              <button type='button' className='btn btn-primary'>
-                <i className='ri-cart-line' />
+              <button type='button' className='btn btn--primary'>
+                <i className='ri-shopping-cart-2-line' />
                 Add to cart
               </button>
             </div>
@@ -41,6 +53,32 @@ function App() {
       </div>
     </div>
   );
+}
+
+function useWindowSize() {
+  // Initialize state with undefined width/height so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+  useEffect(() => {
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    // Add event listener
+    window.addEventListener('resize', handleResize);
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+    // Remove event listener on cleanup
+    return () => window.removeEventListener('resize', handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+  return windowSize;
 }
 
 export default App;
